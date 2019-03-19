20eb349cca196198515be5bdad2687e467936434
Keer Mei <keermei1992@gmail.com>
2019-03-19 15:08:23 +0100

    circuit simulator performance ready for merge'
    '

--- includes/CUnit.h
+++ includes/CUnit.h
@@ -1,16 +1,27 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
 class CUnit {
- public:
-  //index of the unit to which this unitâ€™s concentrate stream is connected 
-  int conc_num;
-  //index of the unit to which this unitâ€™s concentrate stream is connected 
-  int tails_num;
-  //A Boolean that is changed to true if the unit has been seen 
-  bool mark;
+public:
+	int conc_num, tails_num;
+	//index of the unit to which this unit’s concentrate stream is connected 
+	double top[2];
+	//index of the unit to which this unit’s concentrate stream is connected 
+	double bottom[2];
+	//A Boolean that is changed to true if the unit has been seen 
+	bool mark;
 
-  /*
+	double feed[2];
+	double old_feed[2];
+	vector<int> conc_inlets;
+	vector<int> tails_inlets;
+	int conc_count, tail_count;
 
-    ...other member functions and variables of CUnit
-
-  */
+	CUnit();
+	~CUnit();
+	void calc_stream();
+	void store();
+	void wipe_feed();
 };
-
