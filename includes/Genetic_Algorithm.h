4d889bb26e366102a95c0acf5a94f39fc536f61d
Michael Trapp <MT5918@ic.ac.uk>
2019-03-19 17:30:41 +0000

    changed int to double

--- includes/Genetic_Algorithm.h
+++ includes/Genetic_Algorithm.h
@@ -1,3 +1,3 @@
 #pragma once
 
-int Evaluate_Circuit(int *circuit_vector, double tolerance, int max_iterations, int num_units, double conc, double waste, double profit, double cost);
+double Evaluate_Circuit(int *circuit_vector, double tolerance, int max_iterations, int num_units, double conc, double waste, double profit, double cost);
