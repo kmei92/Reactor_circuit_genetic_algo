dee14b63e854cdb9dbf13e100e47b6da829f2a69
unknown <xl16718@ic.ac.uk>
2019-03-20 15:54:31 +0000

    change

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -3,7 +3,7 @@
 #include "CCircuit.h"
 //#include "CCircuit.cpp"
 
-#ifndef NOT_TESTING
+//#ifndef NOT_TESTING
 int main(int argc, char * argv[]){
 
         int valid[21] = { 0,1,2,3,4,5,6,7,8,9,10,10,11,10,11,10,11,10,11,10,11 };
@@ -27,4 +27,4 @@ int main(int argc, char * argv[]){
 */
 
 }
-#endif
+//#endif
