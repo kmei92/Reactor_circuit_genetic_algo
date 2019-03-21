2129b7c8e79a72d9454204a025148327bb6defb0
unknown <xl16718@ic.ac.uk>
2019-03-21 09:47:59 +0000

    change

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -2,6 +2,7 @@
 #include "CUnit.h"
 #include "CCircuit.h"
 #include "Genetic_Algorithm.h"
+//#include "Genetic_Algorithm.cpp"
 
 
 
@@ -16,6 +17,15 @@ int main(int argc, char * argv[]){
 	    std::cout  << "pass\n";
 	else
 	    std::cout << "fail\n";
+
+		int **all_parents;
+		int **new_all_parents;
+		int num_parents = 100;
+		double gene_change_rate = 0.01;
+		double crossover_rate = 0.9;
+
+		double *performance_list;
+		double *distribution;
 
 
 		run_genetic_algorithm(all_parents, new_all_parents, performance_list, distribution, num_parents, num_units);
